77dd391923b84f433654fa0df47622328312ec0d
joel ong <joelong345@gmail.com>
2026-02-07 15:47:07 +0800

    Revert "Merge branch 'improvecode'"
    This reverts commit 77278f66dbab94ae1ba01d5019609a7aba7c92a6, reversing
    changes made to 2755b720ce621a7025d92bc94ce47b2975225908.

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,14 +1,7 @@
-package circus.animal;
-
 public class Duck extends Bird {
     @Override
     public String speak() {
         return "Quack Quack";
-    }
-
-    @Override
-    public int getValue() {
-        return 10;
     }
 
     @Override
