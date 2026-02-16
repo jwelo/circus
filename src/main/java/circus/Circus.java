80c1db4cad2ec18926bb9219f6c39967d7c8ae91
joel ong <joelong345@gmail.com>
2026-02-16 16:47:27 +0800

    Add changes according to lecture

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -47,13 +47,57 @@ public class Circus {
     }
 
     public static void main(String[] args) {
-        makeAnimalsTalk();
-        System.out.println("Total value of animals " + calculateAssetValue(animals));
-        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
-    }
-}
+//        makeAnimalsTalk();
+//        System.out.println("Total value of animals " + calculateAssetValue(animals));
+//        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+//        System.out.println("Numbers of animals in circus: " + animals.length);
+//        for (Animal a: animals) {
+//            System.out.println(a);
+//        }
+        //animals[3] = new Elephant("Strong one");
+//        System.out.println("Numbers of animals in circus: " + animals.length);
+//        for (Animal a: animals) {
+//            System.out.println(a);
+//        }
+        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
+        printNumberOfAnimals(animalArrayList);
+        animalArrayList.add(new Elephant("strong one"));
+        printNumberOfAnimals(animalArrayList);
+        printAllAnimals(animalArrayList);
+        animalArrayList.add(new Duck( "Andy"));
+        Tiger sherKhan = new Tiger("Sher Khan");
+        animalArrayList.add(sherKhan);
+        Parrot bobby = new Parrot("Bobby");
+        animalArrayList.add(bobby);
 
+        System.out.println("Position of sher khan in array list is: " + animalArrayList.indexOf(sherKhan));
+
+        System.out.println("before sorting");
+        printAllAnimals(animalArrayList);
+        animalArrayList.sort(Animal.AnimalNameComparator);
+        System.out.println("after sorting:");
+        printAllAnimals(animalArrayList);
+
+        Animal candidate = findAnimal(animalArrayList,"Polly");
+        System.out.println("Polly is at index: " + animalArrayList.indexOf(candidate));
+    }
 
     private static void printNumberOfAnimals(ArrayList<Animal> animalArrayList) {
         System.out.println("numbers of aniamls in circus (AL)" + animalArrayList.size());
     }
+
+    private static Animal findAnimal(ArrayList<Animal> animalArrayList, String name) {
+        for (Animal a: animalArrayList) {
+            if (a.name == name) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    private static void printAllAnimals(ArrayList<Animal> animalArrayList) {
+        for (Animal a : animalArrayList) {
+            System.out.println(a);
+        }
+    }
+}
